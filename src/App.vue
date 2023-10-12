043b5d40979dd74013e674c83326aa9b41a9806a
nisino25 <nisino25@gmail.com>
2023-10-13 01:29:23 +0900

    added a littl bit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="region">
     <ul>
-      <li><a  @click="scrollToSection('pokemon-1')">カントー</a></li>
-      <li><a  @click="scrollToSection('pokemon-152')">ジョウト</a></li>
-      <li><a  @click="scrollToSection('pokemon-252')">ホウエン</a></li>
-      <li><a  @click="scrollToSection('pokemon-387')">シンオウ</a></li>
-      <li><a  @click="scrollToSection('pokemon-494')">イッシュ</a></li> <br>
-      <li><a  @click="scrollToSection('pokemon-650')">カロス</a></li>
-      <li><a  @click="scrollToSection('pokemon-722')">アローラ</a></li>
-      <li><a  @click="scrollToSection('pokemon-813')">ガラル</a></li>
-      <li><a  @click="scrollToSection('pokemon-899')">ヒスイ</a></li>
-      <li><a  @click="scrollToSection('pokemon-906')">パルデア</a></li>
+      <li style="" @click="scrollToSection('pokemon-1')">
+        <div class="percentage" :style="{ width: (luckyCounts[0] / 151 * 100) + '%' }"></div>
+        カントー<br>{{ luckyCounts[0] }}/151
+      </li>
+      <li @click="scrollToSection('pokemon-152')">
+        <div class="percentage" :style="{ width: (luckyCounts[1] / 100 * 100) + '%' }"></div>
+        ジョウト<br>{{ luckyCounts[1] }}/100
+      </li>
+      <li @click="scrollToSection('pokemon-252')">
+        <div class="percentage" :style="{ width: (luckyCounts[2] / 135 * 100) + '%' }"></div>
+        ホウエン<br>{{ luckyCounts[2] }}/135
+      </li>
+      <li @click="scrollToSection('pokemon-387')">
+        <div class="percentage" :style="{ width: (luckyCounts[3] / 113 * 100) + '%' }"></div>
+        シンオウ<br>{{ luckyCounts[3] }}/113
+      </li>
+      <li @click="scrollToSection('pokemon-494')">
+        <div class="percentage" :style="{ width: (luckyCounts[4] / 146 * 100) + '%' }"></div>
+        イッシュ<br>{{ luckyCounts[4] }}/146
+      </li>
+      <li @click="scrollToSection('pokemon-650')">
+        <div class="percentage" :style="{ width: (luckyCounts[5] / 72 * 100) + '%' }"></div>
+        カロス<br>{{ luckyCounts[5] }}/72
+      </li>
+      <li @click="scrollToSection('pokemon-722')">
+        <div class="percentage" :style="{ width: (luckyCounts[6] / 91 * 100) + '%' }"></div>
+        アローラ<br>{{ luckyCounts[6] }}/91
+      </li>
+      <li @click="scrollToSection('pokemon-813')">
+        <div class="percentage" :style="{ width: (luckyCounts[7] / 86 * 100) + '%' }"></div>
+        ガラル<br>{{ luckyCounts[7] }}/86
+      </li>
+      <li @click="scrollToSection('pokemon-899')">
+        <div class="percentage" :style="{ width: (luckyCounts[8] / 7 * 100) + '%' }"></div>
+        ヒスイ<br>{{ luckyCounts[8] }}/7
+      </li>
+      <li @click="scrollToSection('pokemon-906')">
+        <div class="percentage" :style="{ width: (luckyCounts[9] / 114 * 100) + '%' }"></div>
+        パルデア<br>{{ luckyCounts[9] }}/114
+      </li>
     </ul>
   </div>
   <div>
@@ -37,7 +67,7 @@ export default {
   data(){
     return{
       pokeData: [],
-      currentPoke: null,
+      currentPoke: 99999,
     }
   },
   mounted(){
@@ -78,7 +108,7 @@ export default {
     },
     selectMe(i){
       console.log(i)
-      if(!this.currentPoke || this.currentPoke !== i) {
+      if(this.currentPoke == 99999 || this.currentPoke !== i) {
         this.currentPoke = i;
         console.log('not smae')
         return
@@ -87,7 +117,7 @@ export default {
       if(i == this.currentPoke){
         console.log('changing')
         this.pokeData[i] = !this.pokeData[i]
-        this.currentPoke = null
+        this.currentPoke = 99999
       }
     },
   },
@@ -100,6 +130,38 @@ export default {
       deep: true,
     },
   },
+  computed:{
+    luckyCounts() {
+      let masterarray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]; // Initialize masterarray with two zeros
+      for (let i = 0; i < this.pokeData?.length; i++) {
+        if (this.pokeData[i] === true) {
+          if (i >= 0 && i <= 150) {
+            masterarray[0]++;
+          } else if (i <= 250) {
+            masterarray[1]++;
+          } else if (i <= 385) {
+            masterarray[2]++;
+          } else if (i <= 492) {
+            masterarray[3]++;
+          } else if (i <= 648) {
+            masterarray[4]++;
+          } else if (i <= 720) {
+            masterarray[5]++;
+          } else if (i <= 808) {
+            masterarray[6]++;
+          } else if (i <= 897) {
+            masterarray[7]++;
+          } else if (i <= 904) {
+            masterarray[8]++;
+          }else{
+            masterarray[9]++;
+          }
+          // You can continue the pattern for other ranges
+        }
+      }
+      return masterarray;
+    }
+  }
 }
 </script>
 
@@ -122,6 +184,7 @@ html{
   top: 0px;
   padding-top: 30px;
   background-color:  #333333 ;
+  z-index: 99;
 }
 
 .region ul{
@@ -131,18 +194,38 @@ html{
   width: 95vw;
   margin: auto;
   padding: 0;
+  
 
 } 
 
 .region ul li{
+  position: relative;
   width: calc(20% - 10px);
   margin-bottom: 10px;
   list-style-type: none;
   border: 2px solid black;
+
+  padding:10px 0px;
+  line-height: 1.75;
+
   
 }
 
+.region ul li .percentage{
+  position: absolute;
+  display: block;
+
+  top: 0;
+  left: 0;
+  height: 100%;
+  z-index: 0;
+  background: #DAA520;
+  opacity: 0.3;
+
+}
+
 .wrapper{
+  z-index: 98;
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
